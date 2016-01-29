0025ab336c54b1f6689f375cf1e0c4bdadf849b8
Max Soe <msoe@expedia.com>
2016-01-29 12:03:48 +0000

    1st working iteration

---
+++ guest review filter/background.js
@@ -0,0 +1,14 @@
+//The code that is fired upon page load
+//to check your plugin js is working uncomment the next line.
+//$("body").append('Test');
+
+console.log("Chrome extension is running ");
+
+
+var guestreviewfilter = chrome.extension.getURL("guestreviewfilter.html");
+var mystuff = '<div>Testing my stuff</div><div>2nd set of stuff</div>';
+
+$.get( guestreviewfilter, function( data ) {
+  $("#lodgingTypecontainer").after(data);
+  // alert( "Load was performed." );
+});
