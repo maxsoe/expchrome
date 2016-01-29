dda55bad4766c639cbec31e56497012e140e35a6
Max Soe <msoe@expedia.com>
2016-01-29 12:47:19 +0000

    Booking.com variant with radio buttons

--- guest review filter/background.js
+++ guest review filter/background.js
@@ -2,13 +2,15 @@
 //to check your plugin js is working uncomment the next line.
 //$("body").append('Test');
 
-console.log("Chrome extension is running ");
+console.log("Chrome extension is running");
 
 
-var guestreviewfilter = chrome.extension.getURL("guestreviewfilter.html");
+var guestreviewfilter = chrome.extension.getURL("guestreviewfilter01.html");
 var mystuff = '<div>Testing my stuff</div><div>2nd set of stuff</div>';
 
 $.get( guestreviewfilter, function( data ) {
   $("#lodgingTypecontainer").after(data);
   // alert( "Load was performed." );
+  // var variantdescription = $("div.uxvariant").text;
+  // console.log(variantdescription);
 });
