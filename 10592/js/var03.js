8438773830ab9bf3e810bfd7a11bc2c50888fdfd
Max Soe <msoe@expedia.com>
2016-02-22 14:34:58 +0000

    Get all variants working from pop-up

---
+++ 10592/js/var03.js
@@ -0,0 +1,32 @@
+console.log("Setting variant 3");
+var currentVariant = chrome.extension.getURL("/html/var03.html");
+var description = "Change applied messaging to using language";
+console.log("currentVariant: ", currentVariant);
+// Clear things from other variants
+  $(".hotelWrapper .expediaPoints").remove();
+  $(".hotelWrapper .actualPrice").empty();
+  $(".hotelWrapper .priceType").empty();
+
+// Insert the content into the page
+  $.get( currentVariant, function( myHTML ) {
+    var headContent = $(myHTML)[1];
+    console.log("headContent: ", headContent);
+    $("head").append(headContent);
+
+    var expediaPoints = $(myHTML).filter('.expediaPoints')[0].outerHTML;
+    var actualPrice = $(myHTML).filter('.actualPrice')[0].innerHTML;
+    var priceType = $(myHTML).filter('.priceType')[0].innerHTML;
+
+    console.log("Expedia:", expediaPoints);
+    $(".hotelWrapper .ratingContainer").after(expediaPoints);
+
+    console.log("actualPrice: ", actualPrice);
+    $(".hotelWrapper .actualPrice").html(actualPrice);
+
+    console.log("priceType: ", priceType);
+    $(".hotelWrapper .priceType").html(priceType);
+
+    // Remove points earning
+    $(".hotelWrapper .earnPointsText").empty();
+
+  });
