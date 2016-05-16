53a04688804f624b1cebf9df2e55a14f8404b819
Max Soe <msoe@expedia.com>
2016-05-16 15:25:21 +0100

    First three variants

---
+++ GRO3489/js/var03.js
@@ -0,0 +1,35 @@
+var description = "include per night price";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Clear things from other variants
+    // $(".hotelWrapper .expediaPoints").remove();
+    // $(".hotelWrapper .actualPrice").empty();
+    // $(".hotelWrapper .priceType").empty();
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var multinights = $(myHTML).filter('.multi-nights')[0].outerHTML;
+
+      console.log("multi-nights:", multinights);
+      $(".avg-rate .breakfastSurCharge").before(multinights);
+
+    });
+}
---
+++ GRO3489/js/var09.js
@@ -0,0 +1,45 @@
+var description = "For .com - 'Avg rate per room, per night' before price, and in header. Urgency below price";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Clear things from other variants
+    // $(".hotelWrapper .expediaPoints").remove();
+    // $(".hotelWrapper .actualPrice").empty();
+    // $(".hotelWrapper .priceType").empty();
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var headerrate = $(myHTML).filter('#avg-rate-header')[0].innerHTML;
+      console.log("header rate:", headerrate);
+      $("#avg-rate-header").html(headerrate);
+
+      var avgrateperroom = $(myHTML).filter('.avg-rate-per-room')[0].outerHTML;
+      // var avgrateperroom = $(myHTML).filter('.avg-rate-per-room');
+      console.log("avg rate per room:", avgrateperroom);
+      $(".room-price").before(avgrateperroom);
+
+      var roomsLeft = $(".rooms-left")[0].outerHTML;
+      console.log("roomsLeft:", roomsLeft);
+      $(".rooms-left").remove();
+
+      $(".room-price-info-wrapper").after(roomsLeft);
+
+    });
+}
