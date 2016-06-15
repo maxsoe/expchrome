1f58855565a3bd78805c1289c2b3d2288037f653
Max Soe <msoe@expedia.com>
2016-06-15 16:39:39 +0100

    Variant 1

---
+++ GRO3744/js/var01.js
@@ -0,0 +1,36 @@
+var description = "Bring SRP's search form into infosite";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Clear things from other variants
+    // $(".hotelWrapper .expediaPoints").remove();
+    // $(".hotelWrapper .actualPrice").empty();
+    // $(".hotelWrapper .priceType").empty();
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var wizard = $(myHTML).filter('#wizard')[0].outerHTML;
+
+      // console.log("wizard:", wizard);
+      $("#price-change-container").before(wizard);
+      $("body").addClass("wizard-responsive");
+
+    });
+}
