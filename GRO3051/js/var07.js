7bc1768e48d4e2587f34cad14b183111627c1086
Max Soe <msoe@expedia.com>
2016-06-30 12:07:47 +0100

    Variant 7
    Weight given to total price

---
+++ GRO3051/js/var07.js
@@ -0,0 +1,36 @@
+var description = 'Weight given to total price';
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Change the page's title to match the current variant
+  $("title").text(variant);
+
+  // Clear things from other variants
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var avgrate = $(myHTML).filter('.avg-rate-display')[0].outerHTML;
+      // console.log("wizard:", wizard);
+      $(".price-wrapper").append(avgrate);
+
+      var totalrate = $(myHTML).filter('.total-rate-display')[0].outerHTML;
+      $(".price-wrapper").append(totalrate);
+    });
+}
