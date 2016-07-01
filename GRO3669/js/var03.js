463042767cf7042f38ba208222fcf518b6098d6b
Max Soe <msoe@expedia.com>
2016-07-01 13:54:03 +0100

    Setup

---
+++ GRO3669/js/var03.js
@@ -0,0 +1,32 @@
+var description = "Combination of flexibility and price";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Clear things from other variants
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var paylater = $(myHTML).filter('#paylater')[0].outerHTML;
+
+      // console.log("wizard:", wizard);
+      $("#availability-wizard-container").after(paylater);
+
+    });
+}
