07b08e2ee6ac6be0101fba7f50d5eafb47fa9843
Max Soe <msoe@expedia.com>
2016-08-04 16:18:34 +0100

    Variant 2 - end of page

---
+++ 3PP9447/js/var02.js
@@ -0,0 +1,41 @@
+var description = "Message at end of page";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+    // console.log("var01 got response", response);
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+  // Remove items from page
+    $("#filterForm").empty(); // Filters
+
+  // Insert content into the page
+    $.get( currentVariant, function( myHTML ) {
+      var sortbarDE = $(myHTML).filter('#sortbarDE')[0].outerHTML;
+      console.log("sortbarDE:", sortbarDE);
+      $('#sortbarDE').replaceWith(sortbarDE);
+
+      var divRegionList = $(myHTML).filter('#divRegionList')[0].outerHTML;
+      console.log("divRegionList:", divRegionList);
+      $('section.six .flex-1up').replaceWith(divRegionList);
+
+
+
+      var paybackMessage = $(myHTML).filter('.payback-message')[0].outerHTML;
+      console.log("paybackMessage:", paybackMessage);
+      $('#sortbarDE').before(paybackMessage);
+
+    });
+}
