9fb126ea24863c8f7316b4c08f3f54b9d44b0531
Max Soe <msoe@expedia.com>
2016-08-08 10:41:22 +0100

    Variant 3
    Jerome’s variant

---
+++ LDG11682/js/var03.js
@@ -0,0 +1,29 @@
+var description = "Jerome's design";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var moreOfBrand = $(myHTML).filter('.more-of-brand')[0].outerHTML;
+      $(".whole-listing").append(moreOfBrand);
+
+    });
+}
