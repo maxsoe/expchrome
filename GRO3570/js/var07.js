b93d25f6d9a38524768fdc36e0b41ff45d7ac188
Max Soe <msoe@expedia.com>
2016-08-11 16:09:35 +0100

    Variant 7

---
+++ GRO3570/js/var07.js
@@ -0,0 +1,28 @@
+var description = "Share tools above rooms and rates module - unboxed";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+      var share = $(myHTML).filter('.share3570')[0].outerHTML;
+      $(".address").after(share);
+      $(".show-map-poi-wrapper").addClass("cf");
+    });
+}
