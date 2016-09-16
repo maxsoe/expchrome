4fbefb3e3c07ea4c3a73b78967ba7acf0397cfa4
Max Soe <msoe@expedia.com>
2016-09-16 11:46:42 +0100

    Variant 2
    Change button only on Infosite.
    
    SRP works just like v1.

---
+++ GRO4018/js/var02.js
@@ -0,0 +1,33 @@
+var description = "Change button only";
+
+chrome.runtime.sendMessage({
+  fn: "getVariant"
+}, function(response) {
+  // Get the response from any listeners and play it back
+
+  // Set value of checked to current variant
+  console.log('this.response is ' + response);
+
+  if (response != 0) {
+    console.log("set currentVariant to response");
+    setVariant(response);
+  }
+});
+
+function setVariant(variant) {
+  var currentVariant = chrome.extension.getURL("/html/"+variant +".html");
+
+  console.log("currentVariant: ", currentVariant);
+
+  // Insert the content into the page
+    $.get( currentVariant, function( myHTML ) {
+
+      var leftChevron = $(myHTML).filter('.icon-toggle270')[0].outerHTML;
+      $("#hsrMap .map-heading").prepend(leftChevron);
+
+      var mapClose = $(myHTML).filter('.btn-label')[0].outerHTML;
+
+      $(".map-close .btn-label").replaceWith(mapClose);
+
+    });
+}
